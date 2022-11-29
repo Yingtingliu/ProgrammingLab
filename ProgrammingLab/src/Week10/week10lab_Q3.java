234e41ea4a94804012b386f29e84dc31d561d21d
Yingtingliu <yi999897@gmail.com>
2022-11-29 18:02:02 +0000

    wk10 lab

--- ProgrammingLab/src/Week10/week10lab_Q3.java
+++ ProgrammingLab/src/Week10/week10lab_Q3.java
@@ -1,6 +1,7 @@
 package Week10;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  *  Binary sequences. Lets define a binary sequence as a sequence of ints that contains only
@@ -20,31 +21,79 @@ import java.util.ArrayList;
 
 public class week10lab_Q3 {
 	
-	public static ArrayList<String> binarySeq(int length, int one) {
+	public static void binarySeq(int arr[], int length, int one, int i) {
 		
-		ArrayList<String> resultsList = new ArrayList<String>();
-		if(one==0) {
-			for(int i=0;i<length;i++) {
-				resultsList.add("0");
-			}			
-			return resultsList;
+		if(length ==i && one ==0) {
+			printTheArray(arr, length);
+	        return;
+		} 
+
+		if(one>=1 && i<length) {
 			
-		} else {
-			for(int j=0;j<length;j++) {
-				resultsList.add("0");
-				resultsList.add("1");	
-				one--;
-				length--;
-			}
+			arr[i] = 0;
+			binarySeq(arr, length, one, i+1);	
+//			System.out.println(Arrays.toString(arr));
 			
-			return binarySeq(length,one);
+			arr[i] = 1;
+			binarySeq(arr, length, one-1, i+1);
+//			System.out.println(Arrays.toString(arr));
 		}
+		else if(i<length) {
+			arr[i] = 0;
+			binarySeq(arr, length, one, i+1);	
+//			System.out.println(Arrays.toString(arr));
+			
+		}
+		
 				
+	}
+	
+	
+	
+	static void printTheArray(int arr[], int n)
+	{
+	    for (int i = 0; i < n; i++)
+	    {
+	        System.out.print(arr[i]+" ");
+	    }
+	    System.out.println();
+	}
+	 
+	// Function to generate all binary strings
+	static void generateAllBinaryStrings(int n,
+	                            int arr[], int i)
+	{
+	    if (i == n)
+	    {
+	        printTheArray(arr, n);
+	        return;
+	    }
+
+	    // First assign "0" at ith position
+	    // and try for all other permutations
+	    // for remaining positions
+//	    System.out.println("i="+i+", n="+n +", [i]="+arr[i]);
+	    arr[i] = 0;
+	    generateAllBinaryStrings(n, arr, i + 1);
+ 
+	    // And then assign "1" at ith position
+	    // and try for all other permutations
+	    // for remaining positions
+//	    System.out.println("i="+i+", n="+n +", [i]="+arr[i]);
+	    arr[i] = 1;
+	    generateAllBinaryStrings(n, arr, i + 1);
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int n = 4;		 
+	    int[] arr = new int[n];
+	    int one = 2;
+	 
+	    // Print all binary strings
+//	    generateAllBinaryStrings(n, arr, 0);
+	    binarySeq(arr, n, one, 0);
 
 	}
 
