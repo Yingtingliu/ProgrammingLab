39e4d9726fce0ddf8235938573e37afba9c31507
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-02-05 00:27:30 +0000

    SE_lab1_ok

--- src/SE_Lab1_seit_1/Person.java
+++ src/SE_Lab1_seit_1/Person.java
@@ -3,6 +3,7 @@ import java.io.PrintStream;
 
 public class Person {
 	
+//	In general, internal details should be hidden
 	private	String	name;
 	private	int	dob_d, dob_m, dob_y;	// date of birth
 	private	int	type;	// 1 = Boss, 2 = Worker
@@ -10,6 +11,29 @@ public class Person {
 	private	Person[] worker;
 	private	int	nworkers;
 	
+	public	Person(String n, int d, int m, int y, int type, Person boss )	{
+		this.name = n;
+		this.dob_d = d;
+		this.dob_m = m;
+		this.dob_y = y;
+		this.type = type; // routine coupling
+		
+		this.boss = boss; // bonus question 
+		this.worker = null;
+		// if boss add workers
+		if(type == 1) {
+			this.worker = new Person[10]; // routine coupling
+		}		
+		this.nworkers = 0;
+	}
+	
+	// bonus question 
+	public	Person(String n, int d, int m, int y, int type ) {
+		// null for a worker without boss
+		this(n,d,m,y,type,null);
+	}
+	
+	// this constructor is for question 1&2, please link to Main2 class
 	public	Person(String n, int d, int m, int y)	{
 		name = n;
 		dob_d = d;
@@ -21,19 +45,21 @@ public class Person {
 		nworkers = 0;
 	}
 	
+	
 	public	void print(PrintStream ps)	{
 		
 		ps.print(String.format("%s: born on %02d/%02d/%4d", getName(), getDob_d(), getDob_m(), getDob_y()));
-		if (this.getNworkers() == 1)	{
+		if (this.getType() == 1)	{
 			ps.print(" bosses: ");
 			for (int i = 0; i < getNworkers(); i++)
 				ps.print(getWorker()[i].getName()+" ");
 		}
 		else
-			ps.print(" is bossed by "+getBoss().getName());
+			ps.print(" is bossed by "+ getBoss().getName());
 	}
 
 	
+//	Using encapsulation
 	public String getName() {
 		return name;
 	}
