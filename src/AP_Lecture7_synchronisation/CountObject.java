15496c00b9caf613ea8b52b746fe0d4e01dd3ca4
Ying Ting Liu <2817385L@student.gla.ac.uk>
2023-02-21 13:10:29 +0000

    AP_Lecture code

---
+++ src/AP_Lecture7_synchronisation/CountObject.java
@@ -0,0 +1,10 @@
+package AP_Lecture7_synchronisation;
+public class CountObject {
+	private Integer i = 0;
+	public Integer geti() {
+		return i;
+	}
+	public void seti(int a) {
+		i = a;
+	}
+}
---
+++ src/AP_Lecture7_synchronisation/CountThread.java
@@ -0,0 +1,16 @@
+package AP_Lecture7_synchronisation;
+public class CountThread extends Thread {
+	private int nSteps;
+	private CountObject c;
+	public CountThread(int n,CountObject cin) {
+		nSteps = n;
+		c = cin;
+	}
+	public void run() {
+		for(int i=0;i<nSteps;i++) {
+			Integer a = c.geti();
+			a += 1;
+			c.seti(a);
+		}
+	}
+}
